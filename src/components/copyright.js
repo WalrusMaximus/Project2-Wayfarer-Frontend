b34ee349c6a3da9813fdfb6e6ee64cdfc85c1101
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-04 15:44:28 -0800

    corrects typo in folders and adds empty files to folders

---
+++ src/components/copyright.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class copyright extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default copyright;
