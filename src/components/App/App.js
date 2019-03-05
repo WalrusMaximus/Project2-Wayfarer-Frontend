40e7eb7f8e4c0f6d3937fb6d0855253aaba7bd72
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-05 12:31:37 -0800

    adds react router and corrects file path issues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Header2 from './components/Header2';
-import Copyright from './components/Copyright'
-import Landing from './components/landing/Landing'
+import Header from '../Header';
+import Header2 from '../Header2';
+import Copyright from '../Copyright'
+import Landing from '../landing/Landing'
 import './App.css';
-import ListingContainer from './components/listing/ListingContainer';
+import ListingContainer from '../listing/ListingContainer';
 
 class App extends Component {
   render() {
