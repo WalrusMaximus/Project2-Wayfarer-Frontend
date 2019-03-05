d6e6d9c88a1274679a74a695864d09799e5d7a01
josephlbaker <joelucasbaker@gmail.com>
2019-03-04 18:47:24 -0800

    added nav using semantic react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
+// import ListingContainer from './components/listing/ListingContainer';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <p>Beep Boop</p>
       </div>
     );
---
+++ src/components/header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Button, Dropdown, Menu, Search } from 'semantic-ui-react'
+
+
+export default class Header extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+
+  render() {
+
+    let menuStyle = {
+      borderRadius: 0
+    }
+
+    let searchStyle = {
+      marginLeft: '20px',
+      marginTop: 'auto',
+      marginBottom: 'auto'
+    }
+
+    const { activeItem } = this.state
+    return (
+      <Menu style={menuStyle} stackable inverted>
+        <Menu.Item>
+          <img src='https://react.semantic-ui.com/logo.png' />
+        </Menu.Item>
+
+        <Menu.Item>
+          Wayfarer
+        </Menu.Item>
+
+        <Search style={searchStyle} size='small' />
+
+        <Menu.Menu position='right'>
+          <Dropdown item text='Profile'>
+            <Dropdown.Menu>
+              <Dropdown.Item>View Profile</Dropdown.Item>
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
