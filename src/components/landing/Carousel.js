364a113a2734db5bd21e4f45f4ecda247c4f3547
Matt Freeland <matthewjfreeland@gmail.com>
2019-03-05 12:13:38 -0800

    fixed carousel

--- src/components/landing/Carousel.js
+++ src/components/landing/Carousel.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
-
-class carousel extends Component {
-    render() {
-        return (
-            <div className="carousel">
-                <p>Carousel</p>
-            </div>
-        );
-    }
+import React from "react";
+import Slider from "react-slick";
+import { Grid, Image, Segment } from 'semantic-ui-react'
+class carousel extends React.Component {
+  render() {
+    let settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+        <Slider {...settings}>
+            <Image src='/images/carousel1.jpg' />    
+            <Image src='/images/carousel2.jpg' />
+            <Image src='/images/carousel3.jpg' />
+            <Image src='/images/carousel1.jpg' />
+            <Image src='/images/carousel2.jpg' />
+            <Image src='/images/carousel3.jpg' />
+        </Slider>
+    );
+  }
 }
 
 export default carousel;
