31c1b927ec02bd24b65bad712b09ffee635d7c09
vagarioustoast <vagarioustoast@gmail.com>
2019-03-07 17:44:30 -0800

    broken profile axios call to fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import axios from 'axios'
-import Header from '../Header';
-import Copyright from '../Copyright'
-import Landing from '../landing/Landing'
-import ListingContainer from '../listing/ListingContainer'
-import PostContainer from '../profile/ProfileContainer'
-import './App.css';
-import ProfileContainer from '../profile/ProfileContainer';
-import SignIn from '../SignIn';
+import axios from "axios";
+import Header from "../Header";
+import Copyright from "../Copyright";
+import Landing from "../landing/Landing";
+import ListingContainer from "../listing/ListingContainer";
+import PostContainer from "../profile/ProfileContainer";
+import "./App.css";
+import ProfileContainer from "../profile/ProfileContainer";
+import SignIn from "../SignIn";
 
 class App extends Component {
   state = {
-    name: '',
-    userName: '',
-    email: '',
-    password: '',
+    name: "",
+    userName: "",
+    email: "",
+    password: "",
     isLoggedIn: false,
     user: null,
     redirect: false,
     cities: []
-  }
+  };
 
   componentDidMount() {
     if (localStorage.token) {
       axios({
-        method: 'get',
+        method: "get",
         url: `http://localhost:3001/users/`,
         headers: { authorization: `Bearer ${localStorage.token}` }
       })
@@ -35,88 +35,91 @@ class App extends Component {
           this.setState({
             isLoggedIn: true,
             user: response.data.user // ????? .user?
-          })
+          });
         })
         .catch(err => {
           this.setState({
             isLoggedIn: false
-          })
-          localStorage.clear()
-        })
+          });
+          localStorage.clear();
+        });
     } else {
       this.setState({
         isLoggedIn: false
-      })
+      });
     }
     this.displayListing();
   }
 
-  handleInput = (event) => {
+  handleInput = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
-  handleSignUp = (event) => {
+  handleSignUp = event => {
     event.preventDefault();
-    axios.post('http://localhost:3001/users/signup',
-      {
+    axios
+      .post("http://localhost:3001/users/signup", {
         name: this.state.name,
         userName: this.state.userName,
         email: this.state.email,
         password: this.state.password
       })
       .then(response => {
-        console.log(response)
-        localStorage.token = response.data.signedJwt
+        console.log(response);
+        localStorage.token = response.data.signedJwt;
 
         this.setState({
           isLoggedIn: true,
           user: response.data.user
-        })
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
-  handleLogIn = (event) => {
+  handleLogIn = event => {
     event.preventDefault();
-    console.log('its working!!!!!!!!!!!');
-    axios.post('http://localhost:3001/users/login', { // we need to be able to connect this to heroku as well
-      email: this.state.email,
-      password: this.state.password
-    })
+    console.log("its working!!!!!!!!!!!");
+    axios
+      .post("http://localhost:3001/users/login", {
+        // we need to be able to connect this to heroku as well
+        email: this.state.email,
+        password: this.state.password
+      })
       .then(response => {
-        localStorage.token = response.data.signedJwt
+        localStorage.token = response.data.signedJwt;
         this.setState({
           isLoggedIn: true,
           user: response.data.user,
           redirect: true
-        })
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   handleLogOut = () => {
     this.setState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       isLoggedIn: false
-    })
-    localStorage.clear()
-  }
+    });
+    localStorage.clear();
+  };
 
   displayListing = () => {
-    axios.get('https://damp-citadel-74040.herokuapp.com/cities')
-      .then((res) => {
-        console.log('found listings')
+    axios
+      .get("https://damp-citadel-74040.herokuapp.com/cities")
+      .then(res => {
+        console.log("found listings");
         this.setState({
           cities: res.data
-        })
+        });
       })
       .catch(error => {
-        console.log('Error fetching and parsing data for listings', error);
-    });
-  }
+        console.log("Error fetching and parsing data for listings", error);
+      });
+  };
 
   // displayPosts = () => {
   //   axios.get('https://damp-citadel-74040.herokuapp.com/posts')
@@ -129,9 +132,7 @@ class App extends Component {
   //   });
   // }
 
-
   render() {
-    
     return (
       <div>
         {/* <div>{cityComponents}</div> */}
@@ -142,62 +143,68 @@ class App extends Component {
           handleInput={this.handleInput}
           handleLogOut={this.handleLogOut}
         />
-        <div className='body'>
+        <div className="body">
           <Switch>
-            <Route exact path='/'
-              render={(props) => {
+            <Route
+              exact
+              path="/"
+              render={props => {
                 if (localStorage.token) {
-                  return <Redirect to="/profile" />
+                  return <Redirect to="/profile" />;
                 } else {
                   return (
                     <div>
                       <Landing />
                       <Copyright />
                     </div>
-                  )
-                }
-              }
-              }
-            />
-            <Route path='/listings'
-              render={(props) => {
-                if (localStorage.token) {
-                  return (
-                    <div>
-                      <ListingContainer displayListing={this.displayListing} 
-                      cities={this.state.cities}/>
-                      <Copyright />
-                    </div>
-                  )
-                } else {
-                  return (
-                    <div>
-                      <Landing />
-                      <Copyright />
-                    </div>
-                  )
+                  );
                 }
               }}
             />
-            <Route path='/profile'
-              render={(props) => {
+            <Route
+              path="/listings"
+              render={props => {
                 if (localStorage.token) {
                   return (
                     <div>
-                      <ProfileContainer />
+                      <ListingContainer
+                        displayListing={this.displayListing}
+                        cities={this.state.cities}
+                      />
                       <Copyright />
                     </div>
-                  )
+                  );
                 } else {
                   return (
                     <div>
                       <Landing />
                       <Copyright />
                     </div>
-                  )
+                  );
                 }
               }}
-            /> */}
+            />
+            <Route
+              path="/profile"
+              render={props => {
+                if (localStorage.token) {
+                  return (
+                    <div>
+                      <ProfileContainer displayProfile={this.displayProfile} />
+                      <Copyright />
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div>
+                      <Landing />
+                      <Copyright />
+                    </div>
+                  );
+                }
+              }}
+            />{" "}
+            */}
           </Switch>
         </div>
       </div>
--- src/components/profile/ProfileContainer.js
+++ src/components/profile/ProfileContainer.js
@@ -1,31 +1,57 @@
-import React, { Component } from 'react'
-import { Grid } from 'semantic-ui-react';
-import Profile from './Profile';
-import ProfilePosts from './ProfilePosts';
+import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+import Profile from "./Profile";
+import ProfilePosts from "./ProfilePosts";
+import axios from "axios";
 
 export default class ProfileContainer extends Component {
-  render() {
+  state = {
+    name: "",
 
+    currentlyViewingUser: {
+      name: "",
+      userName: "",
+      email: ""
+    }
+  };
+
+  componentDidMount = () => {
+    this.displayProfile();
+  };
+
+  displayProfile = () => {
+    axios
+      .get("https://damp-citadel-74040.herokuapp.com/users/profile/:id")
+      .then(res => {
+        console.log(res);
+        this.setState({
+          currentlyViewingUser: res.data.user
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
     let gridMargin = {
       margin: "0",
       padding: "1em",
       paddingTop: "2em"
-    }
+    };
 
     let leftGrid = {
       paddingTop: "2em",
       paddingLeft: "3em"
-    }
+    };
 
     return (
       <Grid centered>
         <Grid.Column width={5} style={leftGrid}>
-          <Profile />
+          <Profile showingUser={this.state.currentlyViewingUser} />
         </Grid.Column>
         <Grid.Column width={11} style={gridMargin}>
           <ProfilePosts />
         </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
